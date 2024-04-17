a9502c257f02447da1f07452557564f30223746f
Lama1kid <143178020+Lama1kid@users.noreply.github.com>
2024-04-17 15:17:26 +0800

    Add files via upload

---
+++ lab/lab05/poke.py
@@ -0,0 +1,23 @@
+def tree(label, branches=[]):
+    for branch in branches:
+        assert is_tree(branch), 'branches must be trees'
+    return [label] + list(branches)
+
+def label(tree):
+    return tree[0]
+
+def branches(tree):
+    return tree[1:]
+
+def is_tree(tree):
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+def is_leaf(tree):
+    return not branches(tree)
+t1 = tree(1, [tree(2, [tree(4), tree(5)]), tree(3, [tree(4), tree(5)])])
+print(t1)
